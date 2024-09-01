b731bce55b7ff51388712f639d99e0690192545f
Dom-santos12 <wellington.libertario@gmail.com>
2024-09-01 00:08:17 -0300

    1

--- relogio-digital/assets/js/script.js
+++ relogio-digital/assets/js/script.js
@@ -1,12 +1,21 @@
 const horas = document.getElementById("horas");
-const minutow = document.getElementById("minutos");
+const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
+const Milliseconds = document.getElementById("centesimos");
 
 const relogio = setInterval(function time(){
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
+    let cs = dateToday.getMilliseconds();
+
+    let now = new Date();
+    let performanceTime = performance.now();
+    let Milliseconds = now.getMilliseconds()
+    let centesimo = Milliseconds;
+
+     // <<< (codigo teste)
     
     if (hr < 10) hr = "0" + hr;
 
@@ -14,8 +23,15 @@ const relogio = setInterval(function time(){
     
     if (s < 10) s = "0" + s;
     
+    if (cs) cs = cs/10;
+
+    if (cs) cs = cs.toFixed(0);
+    
+
+    
     
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
+    centesimos.textContent = cs;
 })
